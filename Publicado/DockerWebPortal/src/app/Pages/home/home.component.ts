fbbc3ab965b1b2a68572f1596c7cb9defce2db5b
Jose Antonio Siguas <jperez@soltecn.com>
2019-12-17 14:25:39 -0500

    actualizacion

--- Publicado/DockerWebPortal/src/app/Pages/home/home.component.ts
+++ Publicado/DockerWebPortal/src/app/Pages/home/home.component.ts
@@ -12,27 +12,52 @@ import {homeService} from './home.Service';
 export class HomeComponent implements OnInit {
   data:string;
 
+  public dataBuscarProducto:any[];
+  public dataBuscarProductoR:any[];
+ 
+name:string;
+
+
+  
   constructor(private homeService: homeService) {}
 
-  public dataBuscarProducto:any[];
+  
   
   ruta_imagen = '../../../assets/imagenes_producto/';
   keyword = 'NOMBRE_PRODUCTO';
   states = this.dataBuscarProducto;
+  fetchData = this.dataBuscarProductoR;
 
   dataByName(){
+    
     if (this.data = null) {
       this.data = 'null'
     } 
+
     this.homeService.getBuscarMedicamentoTexto(this.data).subscribe(res=>{
       this.dataBuscarProducto=res[0];
-      console.log(JSON.stringify(this.dataBuscarProducto))
+      // console.log(JSON.stringify(this.dataBuscarProducto))
       this.states = this.dataBuscarProducto;
     });
   }
 
   ngOnInit() {
     this.dataByName();
+  }
+
+  resultado(){
+    console.log("Hola");
+    // console.log("Hola")
+    if (this.name == "") {
+      this.name = "null"
+    } 
+    this.homeService.getBuscarMedicamentoTexto(this.name).subscribe(res=>{
+      this.dataBuscarProductoR=res[0];
+      // console.log(JSON.stringify(this.dataBuscarProductoR))
+      this.fetchData = this.dataBuscarProductoR;
+      this.name = ""
+    });
+
   }
 
 }
